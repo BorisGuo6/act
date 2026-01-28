0eefa7f8f9327e1eb8614784e1ec4a54ea9d9c4b
nolann030628 <borisguo6@gmail.com>
2026-01-28 19:50:11 +0800

    update

--- constants.py
+++ constants.py
@@ -1,10 +1,11 @@
 import pathlib
 
 ### Task parameters
-DATA_DIR = '<put your data dir here>'
+DATA_DIR = '/home/boris/workspace/act/data'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
-        'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
+        # 'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
+        'dataset_dir': DATA_DIR + '/transfer_cube',
         'num_episodes': 50,
         'episode_len': 400,
         'camera_names': ['top']
